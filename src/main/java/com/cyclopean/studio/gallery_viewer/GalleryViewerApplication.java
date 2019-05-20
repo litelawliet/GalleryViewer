3efd6a3d6a74f0292758efe0de42d18ff5d8acdd
kiki <kiliannleclerre@gmail.com>
2019-05-20 20:20:27 +0200

    init project

---
+++ src/main/java/com/cyclopean/studio/gallery_viewer/GalleryViewerApplication.java
@@ -0,0 +1,13 @@
+package com.cyclopean.studio.gallery_viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryViewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyclopean/studio/gallery_viewer/GalleryViewerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cyclopean.studio.gallery_viewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GalleryViewerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
